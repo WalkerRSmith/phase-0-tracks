e092f2cb76c2124f47ad30f86c5c232b4d0c9441
WalkerRSmith <walkerrobinsonsmith@yahoo.com>
2016-11-06 19:00:17 -0600

    Made necessary changes to hashes.rb

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,8 +1,7 @@
-# Ask the user for input
-#
-
+# Create a hash
 interior_design = {}
 
+# Ask the user for input that will fill the hash, describing all the parts of the design
 puts "What is your name?"
 interior_design[:name] = gets.chomp
 
@@ -18,14 +17,15 @@ interior_design[:decor_theme] = gets.chomp
 puts "Would you like us to build a Virtuality Reality tour of the space?"
 interior_design[:vr] = gets.chomp
 
-
+# Ask if the user would like to change any part of the hash
 puts "See anything you would like to change? (name, age, children, decor_theme or vr) If you don't want to change anything, type 'none'."
 new_input = gets.chomp
 
+# Ask if the user would like to change any of the data, print the results with any changes
 if new_input == "none"
 	p interior_design
 else new_input = new_input.to_sym 
-	puts "what is the new value of the key you entered?"
+	puts "what would you like to change that to?"
 	interior_design[new_input] = gets.chomp
 	p interior_design
 end
---
+++ ruby/testerthing.rb
@@ -0,0 +1,7 @@
+count = 0
+
+until count == 11
+	puts "This is still happening"
+	count += 1
+end
+
